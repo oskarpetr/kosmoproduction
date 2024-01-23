14c4357409d9cbc1e9b46faa17d5aa1491ef0657
Oskar Petr <oskarpetr@macoskar.local>
2024-01-23 12:47:32 +0100

    design, favicon, title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { FacebookLogo, PaperPlaneTilt } from "@phosphor-icons/react";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen justify-center items-center select-none">
+      <Head>
+        <title>Kosmo Production</title>
+      </Head>
+
       <motion.div
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -25,10 +31,10 @@ export default function Home() {
         animate={{ opacity: 1 }}
         transition={{ delay: 0.5, ease: "easeInOut" }}
       >
-        <h1 className="text-center text-white text-3xl md:text-5xl font-semibold">
+        <h1 className="text-center text-white text-3xl md:text-4xl font-semibold tracking-wide">
           Coming Soon
         </h1>
-        <p className="text-center text-white text-lg md:text-xl text-opacity-40 w-[20rem] md:w-[32rem]">
+        <p className="text-center text-white text-lg md:text-lg text-opacity-40 w-[20rem] md:w-[32rem] tracking-wide">
           We are coming soon! Currently, we are working hard to give you the
           best experience! See you soon!
         </p>
@@ -43,28 +49,22 @@ export default function Home() {
         <Link
           href="https://www.facebook.com/profile.php?id=61551983007702"
           target="_blank"
+          className="w-12 h-12 flex justify-center items-center bg-white bg-opacity-20 rounded-full"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 24 24"
-            className="fill-neutral-300"
-          >
-            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
-          </svg>
+          <FacebookLogo
+            className="text-white opacity-80 text-3xl"
+            weight="bold"
+          />
         </Link>
 
-        <Link href="mailto:hello@kosmoproduction.eu">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 24 24"
-            className="fill-neutral-300"
-          >
-            <path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z" />
-          </svg>
+        <Link
+          href="mailto:hello@kosmoproduction.eu"
+          className="w-12 h-12 flex justify-center items-center bg-white bg-opacity-20 rounded-full"
+        >
+          <PaperPlaneTilt
+            className="text-white opacity-80 text-2xl"
+            weight="bold"
+          />
         </Link>
       </motion.div>
     </div>
